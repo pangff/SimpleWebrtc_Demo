c9ab7be17a666ffbc6855fce4e78f7245735cc00
“pangff” <admin@example.com>
2016-06-26 12:18:27 +0800

    first init

---
+++ src/server.js
@@ -0,0 +1,11 @@
+var static = require('node-static');
+var https = require('https');
+var fs = require("fs");
+var options = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem')
+};
+var file = new(static.Server)();
+var app = https.createServer(options,function (req, res) {
+  file.serve(req, res);
+}).listen(2013);
